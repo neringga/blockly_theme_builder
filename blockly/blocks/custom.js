557762008705dba743d6a8f3aa1b9fe162875ace
neringga <geigalaite@gmail.com>
2020-11-15 23:23:04 +0200

    Add more blockly elements

--- blockly/blocks/custom.js
+++ blockly/blocks/custom.js
@@ -1,26 +1,28 @@
+
 Blockly.Blocks['front_page'] = {
   init: function() {
+    this.appendDummyInput()
+        .appendField("Front page");
+    this.appendValueInput("margin")
+        .setCheck("content_margin")
+        .appendField("Margins");
     this.appendStatementInput("Front-page")
         .setCheck(null)
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("Front page");
+        .setAlign(Blockly.ALIGN_CENTRE);
     this.setColour(270);
  this.setTooltip("This is a front page block. Connect blocks that you want to appear in your front-page.");
  this.setHelpUrl("");
   }
 };
 
-Blockly.Blocks['posts'] = {
+Blockly.Blocks['page_content'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Posts");
-    this.appendDummyInput()
-        .appendField("Sort:")
-        .appendField(new Blockly.FieldDropdown([["oldest to newest","old_to_new"], ["newest to oldest","new_to_old"], ["random","random"]]), "order_by");
+        .appendField("Assigned page content");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(120);
- this.setTooltip("Child. This block defines your posts. Connects this to parent blocks.");
+ this.setTooltip("");
  this.setHelpUrl("");
   }
 };
@@ -32,7 +34,7 @@ Blockly.Blocks['paragraph'] = {
         .appendField("Paragraph");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(230);
+    this.setColour(120);
  this.setTooltip("Child. Block to add text paragraph.");
  this.setHelpUrl("");
   }
@@ -42,10 +44,14 @@ Blockly.Blocks['header'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Header");
+    this.appendDummyInput()
+        .appendField("Show navigation menu")
+        .appendField(new Blockly.FieldCheckbox("TRUE"), "NAME");
     this.appendStatementInput("header_components")
         .setCheck(null);
+    this.setInputsInline(false);
     this.setColour(270);
- this.setTooltip("Parent. Header that will be shown in all the pages.");
+ this.setTooltip("Header that will be shown in all the pages.");
  this.setHelpUrl("");
   }
 };
@@ -66,6 +72,227 @@ Blockly.Blocks['title'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['text_custom'] = {
+  init: function() {
+    this.appendValueInput("text")
+        .setCheck("String")
+        .appendField("Text:");
+    this.appendValueInput("text_style")
+        .setCheck("text_style")
+        .appendField("Text style:");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['image'] = {
+  init: function() {
+    this.appendValueInput("image_url")
+        .setCheck("String")
+        .appendField("Image url");
+    this.appendDummyInput()
+        .appendField("Opacity")
+        .appendField(new Blockly.FieldNumber(0, 0, 1), "image_opacity");
+    this.appendValueInput("width")
+        .setCheck(["pixels", "percents"])
+        .appendField("Width");
+    this.appendValueInput("height")
+        .setCheck(["pixels", "percents"])
+        .appendField("Height");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['footer'] = {
+  init: function() {
+    this.appendStatementInput("footer_view")
+        .setCheck(null)
+        .appendField("Footer");
+    this.appendDummyInput()
+        .appendField("Show navigation menu")
+        .appendField(new Blockly.FieldCheckbox("TRUE"), "NAME");
+    this.setColour(270);
+ this.setTooltip("Add elements to show in your footer view");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['single_page'] = {
+  init: function() {
+    this.appendStatementInput("single_page_view")
+        .setCheck(null)
+        .appendField("Single page");
+    this.appendValueInput("margin")
+        .setCheck("content_margin")
+        .appendField("Margins");
+    this.setColour(270);
+ this.setTooltip("Add elements that you want to appear in satic page");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['single_post'] = {
+  init: function() {
+    this.appendStatementInput("single_view")
+        .setCheck(null)
+        .appendField("Single post");
+    this.appendValueInput("margin")
+        .setCheck("content_margin")
+        .appendField("Margins");
+    this.setColour(270);
+ this.setTooltip("Add elements to show in your single post view");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['archive'] = {
+  init: function() {
+    this.appendStatementInput("archive_view")
+        .setCheck(null)
+        .appendField("Archive");
+    this.appendValueInput("margin")
+        .setCheck("content_margin")
+        .appendField("Margins");
+    this.setColour(270);
+ this.setTooltip("Add elements to show in your archive (all posts) view");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['navigation'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Navigation")
+        .appendField(new Blockly.FieldDropdown([["Header Menu","header_menu"], ["Footer Menu","footer_menu"], ["Extra Menu","extra_menu"]]), "nav_menu_name");
+    this.appendDummyInput()
+        .appendField("Alignment")
+        .appendField(new Blockly.FieldDropdown([["right","right"], ["left","left"], ["center","center"]]), "navigation_alignment");
+    this.setPreviousStatement(true, ["header", "footer"]);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['text_style'] = {
+  init: function() {
+    this.appendValueInput("font_family")
+        .setCheck("String")
+        .appendField("Font family");
+    this.appendDummyInput()
+        .appendField("Font style")
+        .appendField(new Blockly.FieldDropdown([["Normal","normal"], ["Italic","italic"], ["Oblique","oblique"]]), "font_style");
+    this.appendDummyInput()
+        .appendField("Font weight")
+        .appendField(new Blockly.FieldDropdown([["Normal","normal"], ["Bold","bold"], ["Light","lighter"]]), "font_weight");
+    this.appendDummyInput()
+        .appendField("Font size")
+        .appendField(new Blockly.FieldNumber(12, 0), "font_size")
+        .appendField("px");
+    this.appendValueInput("font_color")
+        .setCheck("Colour")
+        .appendField("Font color");
+    this.appendValueInput("background_color")
+        .setCheck("Colour")
+        .appendField("Background color");
+    this.appendDummyInput()
+        .appendField("Alignment")
+        .appendField(new Blockly.FieldDropdown([["Left","left"], ["Center","center"], ["Right","right"], ["Justify","justify"]]), "alignment");
+    this.appendDummyInput()
+        .appendField("Text transformation")
+        .appendField(new Blockly.FieldDropdown([["None","0"], ["Uppercase","uppercase"], ["Lowercase","lowercase"], ["Capitalize","capitalize"]]), "transformation");
+    this.setOutput(true, null);
+    this.setColour(165);
+ this.setTooltip("Text style");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['posts'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Posts");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Posts per page")
+        .appendField(new Blockly.FieldNumber(10, 1, Infinity, 1), "posts_per_page");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Show only part of the post content")
+        .appendField(new Blockly.FieldCheckbox("TRUE"), "show_part_of_the_content");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Show thumbnail")
+        .appendField(new Blockly.FieldCheckbox("TRUE"), "show_thumbnail");
+    this.appendValueInput("post_title_style")
+        .setCheck("text_style")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Title style");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['pixels'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldNumber(0, 0), "pixels")
+        .appendField("px");
+    this.setOutput(true, null);
+    this.setColour(165);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['percents'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldNumber(0, 0, 100), "percents")
+        .appendField("%");
+    this.setOutput(true, null);
+    this.setColour(165);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['content_margin'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Content margins");
+    this.appendValueInput("left_margin")
+        .setCheck(["pixels", "percents"])
+        .appendField("Left margin");
+    this.appendValueInput("right_margin")
+        .setCheck(["pixels", "percents"])
+        .appendField("Right margin");
+    this.appendValueInput("top_margin")
+        .setCheck(["pixels", "percents"])
+        .appendField("Top margin");
+    this.appendValueInput("bottom_margin")
+        .setCheck(["pixels", "percents"])
+        .appendField("Bottom margin");
+    this.setOutput(true, null);
+    this.setColour(165);
  this.setTooltip("");
  this.setHelpUrl("");
   }
--- blockly/generators/php/custom.js
+++ blockly/generators/php/custom.js
@@ -1,14 +1,18 @@
 
 Blockly.PHP['front_page'] = function(block) {
+  var value_margin = Blockly.PHP.valueToCode(block, 'margin', Blockly.PHP.ORDER_ATOMIC);
   var statements_front_page = Blockly.PHP.statementToCode(block, 'Front-page');
-  // TODO: Assemble PHP into code variable.
-  var code = "<?php get_header();?>" + statements_front_page + "<?php get_footer();?>";
+  
+  var code = `<?php /* Template Name: Front page */ ?>
+              <?php get_header();?>
+              <div ${value_margin}>
+              ${statements_front_page}
+              <?php get_footer();?>
+              </div>`;
   return code;
 };
 
-Blockly.PHP['posts'] = function(block) {
-  var dropdown_order_by = block.getFieldValue('order_by');
-
+Blockly.PHP['page_content'] = function(block) {
   var code = `<?php if (have_posts()) : while(have_posts()) : the_post() ;?>
 
                   <?php the_title(); ?>
@@ -18,26 +22,194 @@ Blockly.PHP['posts'] = function(block) {
   return code;
 };
 
-Blockly.PHP['paragraph'] = function(block) {
-  var value_paragraph_text = Blockly.PHP.valueToCode(block, 'paragraph_text', Blockly.PHP.ORDER_NONE);
-  var code = `<p>${value_paragraph_text}</p>`;
-
-  return code;
-};
-
 Blockly.PHP['header'] = function(block) {
+  var checkbox_name = block.getFieldValue('NAME') == 'TRUE'; // show navigation menu
   var statements_header_components = Blockly.PHP.statementToCode(block, 'header_components');
+  var navigation_menu = "";
+
+  if (checkbox_name == true) {
+    navigation_menu = `<?php wp_nav_menu (
+                          array(
+                            'theme_location' => 'header-menu',
+                            'menu_class' => 'top-bar'
+                          )
+                        ); ?>`
+  }
+  
+  var code = `<!DOCTYPE html>
+  <html>
+    <head><?php wp_head(); ?></head>
+    <body <?php body_class(); ?>>
+
+    <header class="sticky-top">
+    <div class="container">
+      ${navigation_menu}
+      ${statements_header_components}
+      </div>
+    </header>`;
+  return code;
+};
+
+Blockly.PHP['text_custom'] = function(block) {
+  var value_text = Blockly.PHP.valueToCode(block, 'text', Blockly.PHP.ORDER_NONE);
+  var value_text_style = Blockly.PHP.valueToCode(block, 'text_style', Blockly.PHP.ORDER_NONE);
+
+  var code = `<p ${value_text_style}>${removeLiterals(value_text)}</p>`;
+
+  return code;
+};
+
+Blockly.PHP['image'] = function(block) {
+  var value_image_url = Blockly.PHP.valueToCode(block, 'image_url', Blockly.PHP.ORDER_ATOMIC);
+  var number_image_opacity = block.getFieldValue('image_opacity');
+  var value_width = Blockly.PHP.valueToCode(block, 'width', Blockly.PHP.ORDER_ATOMIC);
+  var value_height = Blockly.PHP.valueToCode(block, 'height', Blockly.PHP.ORDER_ATOMIC);
+  
+  var code = `<img src="${removeLiterals(value_image_url)}" alt="Image" style="width:${value_width};height:${value_height};opacity:${number_image_opacity}">`;
+  
+  return code;
+};
+
+Blockly.PHP['footer'] = function(block) {
+  var checkbox_name = block.getFieldValue('NAME') == 'TRUE'; // show navigation menu
+  var statements_footer_components = Blockly.PHP.statementToCode(block, 'footer_components');
+  
+  if (checkbox_name == true) {
+    navigation_menu = `<?php wp_nav_menu (
+                          array(
+                            'theme_location' => 'footer-menu',
+                            'menu_class' => 'bottom-bar'
+                          )
+                        ); ?>`
+  }
+
+  return statements_footer_components;
+};
+
+Blockly.PHP['single_page'] = function(block) {
+  var statements_name = Blockly.PHP.statementToCode(block, 'single_page');
+  var value_margin = Blockly.PHP.valueToCode(block, 'margin', Blockly.PHP.ORDER_NONE);
   // TODO: Assemble PHP into code variable.
-  var code = '...;\n';
+  var code = `<?php /* Template Name: Static page */ ?>
+              <?php get_header();?>
+              <div ${value_margin}>
+                ${statements_name}
+              </div>
+              <?php get_footer();?>`;
   return code;
 };
 
-Blockly.PHP['title'] = function(block) {
-  var dropdown_title_font_style = block.getFieldValue('title_font_style');
+Blockly.PHP['single_post'] = function(block) {
+  var statements_single_post = Blockly.PHP.statementToCode(block, 'single_post');
+  var value_margin = Blockly.PHP.valueToCode(block, 'margin', Blockly.PHP.ORDER_NONE);
+  // TODO: Assemble PHP into code variable.
+  var code = `<?php /* Template Name: Single post */ ?>`;
+  return code;
+};
+
+Blockly.PHP['archive'] = function(block) {
+  var statements_archive = Blockly.PHP.statementToCode(block, 'archive');
+  var value_margin = Blockly.PHP.valueToCode(block, 'margin', Blockly.PHP.ORDER_NONE);
+  // TODO: Assemble PHP into code variable.
+  var code = `<?php /* Template Name: Archive */ ?>`;
+  return code;
+};
+
+Blockly.PHP['text_style'] = function(block) {
+  var value_font_family = Blockly.PHP.valueToCode(block, 'font_family', Blockly.PHP.ORDER_NONE);
+  var dropdown_font_style = block.getFieldValue('font_style');
+  var dropdown_font_weight = block.getFieldValue('font_weight');
+  var number_font_size = block.getFieldValue('font_size');
   var value_font_color = Blockly.PHP.valueToCode(block, 'font_color', Blockly.PHP.ORDER_NONE);
-  var value_title_text = Blockly.PHP.valueToCode(block, 'title_text', Blockly.PHP.ORDER_NONE);
+  var value_background_color = Blockly.PHP.valueToCode(block, 'background_color', Blockly.PHP.ORDER_NONE);
+  var dropdown_alignment = block.getFieldValue('alignment');
+  var dropdown_transformation = block.getFieldValue('transformation');
 
-  var code = `<h3 style="font-family:${dropdown_title_font_style}; color:${value_font_color};">${value_title_text}</h3>`;
+  var text_transform = dropdown_transformation == 0 ? "" : `text-transform:${dropdown_transformation};`;
+
+  var code = `style="font-family:${removeLiterals(value_font_family)}; 
+                     font-size:${number_font_size}px;
+                     font-weight:${dropdown_font_weight};
+                     color:${removeLiterals(value_font_color)};
+                     background-color:${removeLiterals(value_background_color)};
+                     text-align:${dropdown_alignment};
+                     ${text_transform}
+                     font-style:${dropdown_font_style};"`;
+
+  return [code, Blockly.PHP.ORDER_NONE];
+};
+
+Blockly.PHP['posts'] = function(block) {
+  var number_posts_per_page = block.getFieldValue('posts_per_page');
+  var checkbox_show_part_of_the_content = block.getFieldValue('show_part_of_the_content') == 'TRUE';
+  var checkbox_show_thumbnail = block.getFieldValue('show_thumbnail') == 'TRUE';
+  var value_post_title_style = Blockly.PHP.valueToCode(block, 'post_title_style', Blockly.PHP.ORDER_NONE);
+
+  var thumbnail_code = checkbox_show_thumbnail ? `<?php if(has_post_thumbnail()):?>            
+                                                  <a href="<?php the_permalink();?>">
+                                                    <img src="<?php the_post_thumbnail_url();?>" class="img-fluid mb-3" alt="<?php the_title();?>">
+                                                  </a>
+                                                  <?php endif;?>` : "";
+  var content = checkbox_show_part_of_the_content ? "the_excerpt()" : "the_content()";
+  
+  var code = `<?php
+              $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => ${number_posts_per_page}
+              );
+              $_posts = new WP_Query($args);
+              ?>
+              
+              <?php if($_posts->have_posts()):?>
+              
+                <?php while($_posts->have_posts()): $_posts->the_post();?>
+              
+                  ${thumbnail_code}
+              
+                  <a href="<?php the_permalink();?>">
+                  <p ${value_post_title_style}><?php the_title();?></p>
+                  </a>
+                  <?php ${content};?>
+              
+                <?php endwhile;?>
+              
+              <?php endif;?>`;
 
   return code;
 };
+
+Blockly.PHP['pixels'] = function(block) {
+  var number_pixels = block.getFieldValue('pixels');
+
+  var code = `${number_pixels}px`;
+  
+  return [code, Blockly.PHP.ORDER_NONE];
+};
+
+Blockly.PHP['percents'] = function(block) {
+  var number_percents = block.getFieldValue('percents');
+  
+  var code = `${number_percents}%`;
+
+  return [code, Blockly.PHP.ORDER_NONE];
+};
+
+Blockly.PHP['content_margin'] = function(block) {
+  var value_left_margin = Blockly.PHP.valueToCode(block, 'left_margin', Blockly.PHP.ORDER_NONE);
+  var value_right_margin = Blockly.PHP.valueToCode(block, 'right_margin', Blockly.PHP.ORDER_NONE);
+  var value_top_margin = Blockly.PHP.valueToCode(block, 'top_margin', Blockly.PHP.ORDER_NONE);
+  var value_bottom_margin = Blockly.PHP.valueToCode(block, 'bottom_margin', Blockly.PHP.ORDER_NONE);
+  
+  var code = ` style="margin-right:${value_right_margin};
+                     margin-left:${value_left_margin};
+                     margin-top:${value_top_margin};
+                     margin-bottom:${value_bottom_margin};"`;
+
+  return [code, Blockly.PHP.ORDER_NONE];
+};
+
+////////// move
+
+function removeLiterals(text) {
+    return text.substring(1, text.length - 1);
+}
